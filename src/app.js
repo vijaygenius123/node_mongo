93af916f4bfc46fb75b4c68742007d7c9a7c53fb
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-07-08 09:47:20 +0530

    (feature) create new listing

--- src/app.js
+++ src/app.js
@@ -7,7 +7,13 @@ async function main(){
 
     try{
         await client.connect()
-        await listDatabases(client)
+        // await listDatabases(client)
+        await createListing(client, {
+            name: "Lovely Loft",
+            summary: "A charming loft in Paris",
+            bedrooms: 1,
+            bathrooms: 1
+        })
     }catch (err){
         console.error(err)
     } finally {
@@ -18,6 +24,11 @@ async function main(){
 async function listDatabases(client){
     const databasesList = await client.db().admin().listDatabases()
     databasesList.databases.forEach(db => console.log(db))
+}
+
+async function createListing(client, newListing){
+    const result = await client.db("sample_airbnb").collection("listingAndReviews").insertOne(newListing)
+    console.log(`New listing created with ${result.insertedId}`)
 }
 
 main().catch(e => console.error(e))
